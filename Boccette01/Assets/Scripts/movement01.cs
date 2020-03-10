8a41a9d0b1e075f625c9a2fb38d0670201877f59
Giovanni Burresi <giovanniburresi@gmail.com>
2020-03-10 11:00:52 +0100

    first commit

---
+++ Boccette01/Assets/Scripts/movement01.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement01 : MonoBehaviour {
+
+	[SerializeField]
+	float speed = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float horMov = Input.GetAxis("Horizontal");
+		float verMov = Input.GetAxis("Vertical");
+
+		Vector3 direction = new Vector3 (0, 0, 0);
+
+		direction.z = horMov;
+		direction.y = verMov;
+
+		transform.Translate (direction * (speed * Time.deltaTime));
+
+	}
+}
---
+++ Boccette01/Assets/Scripts/test01.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test01 : MonoBehaviour 
+{
+	[SerializeField]
+	GameObject cubePrefab;
+
+	// Use this for initialization
+	void Start() 
+	{
+		//cubePrefab = GameObject.Find("Culo");
+		Debug.Log("Cane cane = Spawned by: " + gameObject.name, gameObject);	
+		Instantiate( cubePrefab );
+	}
+	
+	// Update is called once per frame
+	void Update() 
+	{
+		if (Input.GetMouseButtonUp(0)) {
+			Instantiate( cubePrefab, 
+				         new Vector3( Random.Range(-5, 5),
+				                       Random.Range(-5, 5),
+				                       Random.Range(-5, 5)),
+				         cubePrefab.transform.rotation
+			 	);
+		}
+	}
+}
